5d7626376c7eb521acc2308f8dd32b047411c969
dmeskis <dmeskis@gmail.com>
2018-10-17 10:12:54 -0600

    Creates station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,17 @@
 class Station
+  attr_reader :name,
+              :type,
+              :distance,
+              :address,
+              :fuel_type,
+              :access_times
+
+  def initialize(attributes = {})
+    @name         = attributes[:name]
+    @type         = attributes[:type]
+    @distance     = attributes[:distance]
+    @address      = attributes[:address]
+    @fuel_type    = attributes[:fuel_type]
+    @access_times = attributes[:access_times]
+  end
 end
