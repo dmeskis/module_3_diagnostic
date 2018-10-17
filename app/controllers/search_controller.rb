093e274301248f7816b910fc51982efde6c1e511
dmeskis <dmeskis@gmail.com>
2018-10-17 10:10:59 -0600

    Set up faraday in controller to retrieve station info from the API. Parsed json into an array of station hashes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
+    zip = params[:q]
+    @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["x-api-key"] = ENV["API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&fuel_type=ELEC,LPG&limit=10")
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    binding.pry
   end
 end
