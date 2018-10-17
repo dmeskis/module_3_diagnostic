6b6cd1ff4b274c981c7fc644992154249d68182b
dmeskis <dmeskis@gmail.com>
2018-10-17 10:43:31 -0600

    Tests pass

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,12 @@
 class SearchController < ApplicationController
   def index
-    zip = params[:q]
-    @stations = NrelService.new.get_url("/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&radius=6&fuel_type=ELEC,LPG&limit=10")
-    @stations.map! do |station|
-      Station.new(station)
-    end
+    zip = params[:zip]
+    @stations = NrelService.new.get_stations(zip)
     @stations.sort_by! {|station| station.distance}
   end
+
+  private
+    def search_params
+      params.permit(:zip)
+    end
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,10 +1,17 @@
 class NrelService
 
+  def get_stations(zip)
+    stations = get_url("/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&radius=6&fuel_type=ELEC,LPG&limit=10")
+    stations.map do |station|
+      Station.new(station)
+    end
+  end
+
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
-  
+
   private
     def conn
       Faraday.new(url: "https://developer.nrel.gov") do |faraday|
@@ -12,6 +19,4 @@ class NrelService
         faraday.adapter Faraday.default_adapter
       end
     end
-
-
 end
